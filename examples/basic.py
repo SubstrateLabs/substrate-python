b494e42e75ff1a724fe0f623fd2860dad229c294
Liam Griffiths <liamgriffiths@gmail.com>
2024-06-18 15:40:18 -0400

    Implements basic streaming requests

--- examples/basic.py
+++ examples/basic.py
@@ -10,17 +10,21 @@ api_key = os.environ.get("SUBSTRATE_API_KEY")
 if api_key is None:
     raise EnvironmentError("No SUBSTRATE_API_KEY set")
 
-from substrate import Substrate, GenerateText
+from substrate import Substrate, GenerateText, sb
 
 substrate = Substrate(api_key=api_key, timeout=60 * 5)
 
 story = GenerateText(prompt="tell me a story")
-# summary = GenerateText(prompt=sb.concat("Summarize this story: ", story.future.text))
+summary = GenerateText(prompt=sb.concat("Summarize this story: ", story.future.text))
 
-# response = substrate.run(story, summary)
-response = substrate.run(story)
+response = substrate.run(story, summary)
 print(response)
 
+print("=== story")
+story_out = response.get(story)
+print(story_out.text)
+
+print("=== summary")
 summary_out = response.get(summary)
 print(summary_out.text)
 
