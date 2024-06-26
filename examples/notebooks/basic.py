c28eb37488d00b700835fa6a68b615f1e4f16184
benzguo <ben@substrate.run>
2024-06-25 22:54:45 -0700

    update basic example

--- examples/notebooks/basic.py
+++ examples/notebooks/basic.py
@@ -18,84 +18,39 @@ def __():
     return api_key, base64, json, mo, os
 
 
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md("Initialize the Substrate client.")
-    return
-
-
 @app.cell
 def __(api_key):
-    from substrate import Substrate, GenerateText, sb
+    from substrate import Substrate, ComputeText, Box, If, sb
 
     substrate = Substrate(api_key=api_key)
-    return GenerateText, Substrate, sb, substrate
+    return Box, ComputeText, If, Substrate, sb, substrate
 
 
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md(
-        "Generate a story using the [`GenerateText`](https://www.substrate.run/nodes#GenerateText) node."
-    )
+@app.cell
+def __(Box, ComputeText):
+    story = ComputeText(prompt="tell me a story")
+    box = Box(value={
+        "story": story.future.text
+    })
+    return box, story
+
+
+@app.cell
+def __(box, story, substrate):
+    res = substrate.run(story, box)
+    return res,
+
+
+@app.cell
+def __(box, mo, res):
+    mo.tree(res.get(box).value)
     return
 
 
 @app.cell
-def __(GenerateText):
-    story = GenerateText(prompt="tell me a story")
-    return story,
-
-
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md(
-        "Summarize the output of the `story` node using another `GenerateText` node. Because `story` has not yet been run, we use `sb.concat` to work with its future output"
-    )
+def __(mo, res, story):
+    mo.md(res.get(story).text)
     return
-
-
-@app.cell
-def __(GenerateText, sb, story):
-    summary = GenerateText(
-        prompt=sb.concat(
-            "summarize this story in one sentence: ", story.future.text
-        )
-    )
-    return summary,
-
-
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md(
-        "Run the graph chaining `story` → `summary`. This is a simple example, but you can easily build arbitrarily complex branching workflows."
-    )
-    return
-
-
-@app.cell
-def __(story, substrate, summary):
-    response = substrate.run(story, summary)
-    return response,
-
-
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md("Get the output of the summary node by passing it to `response.get`.")
-    return
-
-
-@app.cell
-def __(mo, response, summary):
-    summary_out = response.get(summary)
-    mo.md(summary_out.text)
-    return summary_out,
-
-
-@app.cell
-def __(mo, story, substrate, summary):
-    viz_url = substrate.visualize(story, summary)
-    mo.md(f"[Visualize the graph]({viz_url})")
-    return viz_url,
 
 
 if __name__ == "__main__":
