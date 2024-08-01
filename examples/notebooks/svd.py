a9ae35b1933e1232ee2ff8f838156df503d30097
benzguo <ben@substrate.run>
2024-07-31 22:36:16 -0400

    update svd notebook

--- examples/notebooks/svd.py
+++ examples/notebooks/svd.py
@@ -13,6 +13,7 @@ def __():
 
     api_key = os.environ.get("SUBSTRATE_API_KEY")
     api_key = api_key or "YOUR_API_KEY"
+    print(api_key)
     return api_key, json, mo, os
 
 
@@ -20,7 +21,7 @@ def __():
 def __(api_key):
     from substrate import Substrate
 
-    substrate = Substrate(api_key=api_key, base_url="https://api-staging.substrate.run")
+    substrate = Substrate(api_key=api_key, base_url="https://api.substrate.run")
     return Substrate, substrate
 
 
@@ -28,15 +29,13 @@ def __(api_key):
 def __(substrate):
     from substrate import GenerateImage, UpscaleImage, StableVideoDiffusion
 
-    prompt = "aerial shot of rainforest at sunset clouds sun rays"
-    image_node = GenerateImage(prompt=prompt)
-    upscale_node = UpscaleImage(prompt=prompt, 
-                                output_resolution=2048,
-                                image_uri=image_node.future.image_uri)
-    video_node = StableVideoDiffusion(image_uri=upscale_node.future.image_uri, 
-                                      store="hosted", 
-                                      motion_bucket_id=20, 
-                                      fps=10
+    prompt = "aerial shot of desert at sunset under clouds"
+    image_node = GenerateImage(
+        prompt=prompt,
+        seed=888,
+    )
+    video_node = StableVideoDiffusion(
+        image_uri=image_node.future.image_uri, store="hosted", motion_bucket_id=20, fps=10
     )
 
     res = substrate.run(video_node)
@@ -47,23 +46,28 @@ def __(substrate):
         image_node,
         prompt,
         res,
-        upscale_node,
         video_node,
     )
 
 
 @app.cell
-def __(mo, res, video_node):
+def __(res):
+    print(res.request_id)
+    print(res)
+    return
+
+
+@app.cell
+def __(res, video_node):
     video = res.get(video_node).video_uri
-    mo.image(video)
+    print(video)
     return video,
 
 
 @app.cell
-def __(mo, res, upscale_node):
-    image = res.get(upscale_node).image_uri
-    mo.image(image)
-    return image,
+def __(mo, video):
+    mo.image(video)
+    return
 
 
 if __name__ == "__main__":
